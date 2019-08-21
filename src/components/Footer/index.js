fac589d2912cdd05300e75a42b7eeb2a63a97584
denisprado <denisforigo@gmail.com>
2019-08-21 17:02:55 -0300

    authentication

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,7 +9,9 @@ const Footer = () => (
         <Link to="/">Chun Fotografia</Link>
       </Copyright>
       <MadeBy>
-        <Link to={process.env.REACT_APP_ADMIN_URL}>admin</Link>
+        <a href={`//${process.env.REACT_APP_ADMIN_URL}`}
+          target="_blank"
+          rel="noopener noreferrer">admin </a>
         <a href="http://www.denisforigo.com">website por Denis Forigo</a>
       </MadeBy>
     </Content>
